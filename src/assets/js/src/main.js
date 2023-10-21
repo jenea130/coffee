2240234c4d6ebe4e2cdbf895e048b5ca494f5278
jenea130 <jenea130@mail.ru>
2023-10-21 22:37:56 +0300

    upd: first commit

---
+++ src/assets/js/src/main.js
@@ -0,0 +1,7 @@
+import toggleMenu from "./modules/header/toggleMenu";
+
+window.addEventListener("DOMContentLoaded", function () {
+    console.log("some thing for me");
+    toggleMenu();
+});
+
---
+++ src/assets/js/src/modules/header/toggleMenu.js
@@ -0,0 +1,16 @@
+export default function toggleMenu(){
+    const mainMenu = document.querySelector('#js-main-menu');
+    // const mainMenu = document.querySelector('.menu__list');
+    const sandwichWrap = document.querySelector('#js-sandwich-wrap');
+
+    sandwichWrap.addEventListener('mousedown', function (e) {
+        e.preventDefault();
+
+        if (mainMenu.classList.contains('fixed')) {
+            mainMenu.classList.remove('fixed');
+        } else {
+            mainMenu.classList.add('fixed');
+        }
+        sandwichWrap.classList.toggle('sandwich--active');
+    });
+}
